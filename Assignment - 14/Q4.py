ed99cf8c0e98c4fbea44b526dad8e88a6ce8d549
Ashu-k10 <ashutoshkadu6@gmail.com>
2026-07-02 00:08:44 +0530

    error

--- Assignment - 14/Q4.py
+++ Assignment - 14/Q4.py
@@ -1,4 +1,4 @@
-# Write a lambda function which accepts two number 
+# Write a lambda function which accepts two number and returns minimum number.
 
 Min = lambda No1,No2 : No1 if No1 < No2 else No2
 
